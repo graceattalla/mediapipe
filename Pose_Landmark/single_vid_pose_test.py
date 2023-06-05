cc9f5cbe45e4a1fc1e9b7aea924035a75729edd3
graceattalla <grace.attalla@gmail.com>
2023-06-05 17:11:49 -0600

    Pose detection

---
+++ Pose_Landmark/single_vid_pose_test.py
@@ -0,0 +1,5 @@
+import mediapipe_process_pose as p
+
+VIDEO = r"C:\Users\grace\OneDrive\Surface Laptop Desktop\BCI4Kids\Mediapipe\Videos\Pose\Cropped\P19\P19_B_post_cropped_6s_720p.mp4"
+
+p.process_video(VIDEO)
