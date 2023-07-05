80b28af268bf3302ec7c7236799613458233c19b
graceattalla <95546691+graceattalla@users.noreply.github.com>
2023-07-05 10:34:21 -0600

    model compare

--- Analysis/compare_models.py
+++ Analysis/compare_models.py
@@ -8,12 +8,6 @@ import os
 import numpy as np
 from matplotlib import style
 
-path = r"C:\Users\grace\Documents\Fatigue Study\Fatigue Videos\Rotated Videos\Rotated (Mediapipe)\MediaPipe Done\B&B % Frames\Hand Legacy Percentage Filled 0.2d 0.7t Part. Avg.xlsx"
-
-data=pd.read_excel(path, index_col=0)
-
-print(data.head())
-
 #Plot percent frames w landmarks for all videos (single subplot can be saved)
 def graph_folder_allvideos_subplot(folder):
     parent_folder = os.listdir(folder)
@@ -93,8 +87,7 @@ def threshold(folder, threshold):
     plt.show()
 
 #Bar charts of number of participants with percentage of frames with landmarks above different thresholds
-
-def threshold_subplot(folder, thres):
+def threshold_subplot(folder, thres): #threshold is a list
     parent_folder = os.listdir(folder)
     fig, axs = plt.subplots(len(thres), 1, figsize=(14, len(thres)*3), tight_layout=True)
     i = 0
@@ -121,7 +114,50 @@ def threshold_subplot(folder, thres):
     fig_manager.window.title('Num Frames with Landmark Percent Above Thresholds')
     plt.show()
 
+#Plot difference between hand model percentage for every video.
+#To Do:
+# y label is too long- just take model part of file name
+# x label is too squished with indicies
+def allvideos_model_diff(folder): #Folder should contain 2 models to compare. Folder name should describe the models (e.g., "Hand Models")
+    parent_folder = os.listdir(folder) #list of what is in folder (not full path of files in folder)
+    print(parent_folder)
+    folder_name = os.path.basename(os.path.normpath(folder))
+    # fig, axs = plt.subplots(4, 1, figsize=(8, 4*4), tight_layout=True)
+    i = 0
+    y = []
+    x = []
+    file_names = [] #keep track of the order of this list
+    for file in parent_folder:
+        if ".xlsx" in file:
+            file_path = os.path.join(folder, file)
+            file_name = os.path.splitext(file)[0]
+            data=pd.read_excel(file_path, index_col=0)
+            x=data.index
+            file_names.append(file_name)
+            y.append(data["Percent Frames with Landmark"]) #will subtract these later
+
+    y_diff = y[0] - y[1]
+    sns.barplot(x=x, y=y_diff)
+    plt.xlabel("Videos")
+    plt.ylabel(f"Percent Frames with Landmarks Difference ({file_names[0]} - {file_names[1]})") #this is too long of a name right now (need to just get model name from the csv title)
+    plt.title(f"Percent Frames with Landmarks Difference- {folder_name}")
+    fig_manager = plt.get_current_fig_manager()
+    fig_manager.window.title(f"Percent Frames with Landmarks Difference- {folder_name}")
+    plt.show()
+
+#Bar chart of number of participant pairs (pre and post) both with percentage of frames with landmarks above threshold of 30%
+
+#Loop through row of csv and determine if there is a pair.
+#Determine if both of the pairs are above the threshold.
+#If so, add to a counter.
+
+def pair_threshold(file_path): #file to analyze
+    data=pd.read_excel(file_path, index_col=0)
+    for 
+
+
 folder = r"C:\Users\grace\Documents\Fatigue Study\Fatigue Videos\Rotated Videos\Rotated (Mediapipe)\MediaPipe Done\B&B % Frames"
-threshold_subplot(folder, [30, 40, 50, 60])
+# threshol_subplot(folder, [30, 40, 50, 60])
 # graph_folder_allvideos_subplot(r"C:\Users\grace\Documents\Fatigue Study\Fatigue Videos\Rotated Videos\Rotated (Mediapipe)\MediaPipe Done\B&B % Frames")
 # graph_folder_avgparticipant_subplot(folder)
+allvideos_model_diff(r"C:\Users\grace\Documents\Fatigue Study\Fatigue Videos\Rotated Videos\Rotated (Mediapipe)\MediaPipe Done\B&B % Frames\Model B&B %\Hand Models")
--- Analysis/tempCodeRunnerFile.py
+++ Analysis/tempCodeRunnerFile.py
@@ -1,16 +1,1 @@
-bplots(4) #2 rows
-
-# x1=data.index
-# y1=data["Percent Frames with Landmark"]
-
-# mask = data["Average of Each Participant"].notnull()
-# x2 = x1[mask]
-# y2=data.loc[mask, "Average of Each Participant"]
-
-# axs[0].bar(x1, y1)
-# axs[0].set_title('Percent Frames w Landmarks- All Videos')
-
-# axs[1].bar(x2, y2)
-# axs[1].set_title('Percent Frames w Landmarks- Per Participant')
-
-# plt.show()
+e
