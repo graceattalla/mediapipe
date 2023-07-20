f9d943070d77bbecff410d14a3e98b9c3968e5f6
graceattalla <grace.attalla@gmail.com>
2023-07-20 15:06:37 -0600

    combine outputs
    finish and folder function

--- Analysis/combine_outputs.py
+++ Analysis/combine_outputs.py
@@ -14,10 +14,35 @@ import re
 #- could make filling of csv from file a seperate function
 #- NEED TO SWAP WHERE HOLISTIC ACTUALLY SAVES!!!! Save to right if left header name.
 
-def combine_outputs_folder(folder1, folder2): #these should be the parent folders for two different models. Assume folder1 will take preference if data for frame in both.
-    pass
+def combine_outputs_folder(folder1, folder2, combined_save_path): #these should be the parent folders for two different models. Assume folder1 will take preference if data for frame in both.
+    parent_folder1 = os.listdir(folder1)
+    parent_folder2 = os.listdir(folder2)
+    for (inner1, inner2) in zip(parent_folder1, parent_folder2): #inner folders are participant folders. Assumed participant folders in same order for both folders.
+        participant1 = os.path.basename(inner1)
+        participant2 = os.path.basename(inner2)
+        if inner1 == inner2: #extra check if using same participant folder
+            combined_full_save_path = os.path.join(combined_save_path, inner1)
+            os.makedirs(combined_full_save_path)
+            
+            inner1_full = os.path.join(folder1, inner1) #full path
+            inner2_full = os.path.join(folder2, inner2)
+            if os.path.isdir(inner1_full): #check if it is a folder
+                inner_files1 = os.listdir(inner1_full) #list of files in inner participant folder
+                inner_files1_BB = [filename for filename in inner_files1 if "B&B" in filename]#list of files for one participant with B&B in file (files to combine)
+            if os.path.isdir(inner2_full):
+                inner_files2 = os.listdir(inner2_full)
+                inner_files2_BB = [filename for filename in inner_files2 if "B&B" in filename]#list of files for one participant
 
-def combine_outputs(file1, file2): #two files for same video but from different models. Assumes 1 is hand and 2 is holistic
+            for file1 in inner_files1_BB: #iterate through all files
+                for file2 in inner_files2_BB:
+                    if matching_key_file(file1, file2): #files are of same participant and trial (e.g., P01_A_pre)
+                        file1_path = os.path.join(inner1_full, file1)
+                        file2_path = os.path.join(inner2_full, file2)
+                        # print(file1_path, file2_path)
+                        combine_outputs(file1_path, file2_path, participant1, combined_full_save_path)
+
+
+def combine_outputs(file1, file2, participant, combined_full_save_path): #two files for same video but from different models. Assumes 1 is hand and 2 is holistic
 
     file1_df = pd.read_csv(file1, index_col = 0) #set the frame number to be the index column. Use loc to access this
     file2_df = pd.read_csv(file2, index_col = 0)
@@ -44,41 +69,11 @@ def combine_outputs(file1, file2): #two files for same video but from different 
     all_keys.extend(hand_model_info)
     all_keys.extend(hand_keys)
 
+    #STEP 2.a: Check if the right hand is full in file1. If so, fill with file 1 and label model column. (Assuming model1 takes priority)
+    #STEP 2.b: Check if right hand is full in file2. If so and 1.a false, fill with file2 and label model colummn. If 1.a true, just update model column
+    #STEP 2.c: (if 2.b false) Leave as empty (not detected for either) and just fill with index
+    #STEP 3: Repeat Step 2 with left hand
 
-#STEP 2.a: Check if the right hand is full in file1. If so, fill with file 1 and label model column. (Assuming model1 takes priority)
-
-#for index in all indicies
-    #if index row in file1 has values:
-        #for header, value in header, value
-            #if this value is left
-                #if also has a value
-                    #fill new csv with values.
-                    #fill left model1 with a 1
-                #else
-                    #fill with 0
-            #if this value is right
-                # if also has a value
-                    #fill new csv with values.
-                    #fill right model1 with a 1
-                #else
-                    #fill with 0
-
-    #if index row in file2 has a value
-        #for header, value in header, value
-            #if the value is left
-                #if not none:
-                    #if new csv not filled yet
-                        #fill with values
-                        #fill model with 1
-                    #else
-                        #fill with 0
-            #if value is right
-                #if not none:
-                    #if new csv not filled yet
-                    #fill with values
-                    #fill model with 1
-                #else:
-                    #fill with 0
     for master_index in index_list:
     # for master_index, non_row in enumerate(file1_df.iterrows()): #index should be same for file1 and file2
         d_frame = {key: None for key in all_keys} #initialize new row to all None
@@ -112,84 +107,93 @@ def combine_outputs(file1, file2): #two files for same video but from different 
                     match_key = matching_key(header, all_keys) #not exact matches between each model so need to check
                     d_frame["Holistic Left"] = 1
 
-                    if d_frame[match_key] != None: #has not been filled already by file 1
+                    if d_frame[match_key] == None: #has not been filled already by file 1
                         d_frame[match_key] = value
                 else: #nothing detected
                     d_frame["Holistic Left"] = 0
 
             if "Left" in header:
                 if pd.notna(value): #the second model has a value
-                    match_key = matching_key(header, all_keys) #not exact matches between each model so need to check
+                    match_key = matching_key_swap(header, all_keys) #not exact matches between each model so need to check
                     d_frame["Holistic Right"] = 1
 
-                    if d_frame[match_key] != None: #has not been filled already by file 1
+                    if d_frame[match_key] == None: #has not been filled already by file 1
                         d_frame[match_key] = value
+                        # print(d_frame[match_key])
+                        # print(match_key)
+                        # print(f"header: {header}")
+                        # print(master_index)
+                        # print(d_frame)
+
                 else: #nothing detected
                     d_frame["Holistic Right"] = 0
         df_list.append(d_frame)
         print(master_index)
     output_df = pd.DataFrame(df_list)
-    save_path = r"C:\Users\grace\Documents\Fatigue Study\Fatigue Videos\Combined Output Test" + "\combined_output.csv"
+
+    non_none_rows = output_df.iloc[:, 6:].notna().any(axis=1).sum()
+    percent_filled = (round(non_none_rows/output_df.shape[0], 2))*100
+    print(f"non-None row: {non_none_rows}")
+    print(f"% data rows: {non_none_rows/output_df.shape[0]}")
+
+    base_name = extract_file_name(file1)
+
+    save_path = combined_full_save_path + f"\combined_B&B_{percent_filled}%_{base_name}.csv" #save to new folder with participant ID folder
     output_df.to_csv(save_path)
 
-
-                    #if corresponding value in dictionary is not filled yet. dict keye.g., x Image 2 Right
-
-            
-
-        # for index, row in file1_df.iloc[1:].iterrows(): #skip the first row because it is the header
-        #     if row.notna().any(): # check if at least one cell is filled
-        #         for header, value in zip(file1_df.columns, row):
-        #             if "Right" in header:
-        #                 if value != None: #something is detected for the right hand
-        #                     if header in all_keys: #this is something we want to transfer to the new csv
-        #                         d_frame[header] = value
-        #                     # print(header, value)
-        #     d_frame["Index"] = index
-        # print(d_frame)
-
-#for row in rows of file1
-    #if not none (not includindg the index)
-        #if not none in columns with right header
-            #fill the new csv with the according coordinates
-            #assign the file1 model to be a 1 for the right hand
-    # for master_index in index_list:
-    #     d_frame = {key: None for key in all_keys} #initialize new row to all None
-    #     d_frame["Index"] = master_index
-    #     for index, row in file1_df.iloc[1:].iterrows(): #skip the first row because it is the header
-    #         if row.notna().any(): # check if at least one cell is filled
-    #             for header, value in zip(file1_df.columns, row):
-    #                 if "Right" in header:
-    #                     if value != None: #something is detected for the right hand
-    #                         if header in all_keys: #this is something we want to transfer to the new csv
-    #                             d_frame[header] = value
-    #                         # print(header, value)
-    #         d_frame["Index"] = index
-    #     print(d_frame)
-
-#STEP 2.b: Check if right hand is full in file2. If so and 1.a false, fill with file2 and label model colummn. If 1.a true, just update model column
-#STEP 2.c: (if 2.b false) Leave as empty (not detected for either) and just fill with index
-#STEP 3: Repeat Step 2 with left hand
-
 def matching_key(header, all_keys): #return the key in the dictionary that cooresponds to the header given. Necessary because of changed naming convention
-    header_cor, header_num = get_pattern(header)
+    header_cor, header_num, header_hand = get_pattern_cor(header)
     for key in all_keys:
-        key_cor, key_num = get_pattern(key)
-        if key_cor == header_cor and key_num == header_num:
+        key_cor, key_num, key_hand = get_pattern_cor(key)
+        if key_cor == header_cor and key_num == header_num and header_hand == key_hand:
+            return key
+        
+def matching_key_swap(header, all_keys): #return the key in the dictionary that cooresponds to the header given. If the header is left then use the right dictionary label (swapped labelling convention of holistic model)
+    header_cor, header_num, header_hand = get_pattern_cor(header)
+    for key in all_keys:
+        key_cor, key_num, key_hand = get_pattern_cor(key)
+        if key_cor == header_cor and key_num == header_num and header_hand != key_hand: #swap left/right
             return key
 
-def get_pattern(string):
+def get_pattern_cor(string):
     cor_pattern = r'[xyz]' #x,y, or z
     num_pattern = r'(\d{1,2})' #2 digit number
+    hand_pattern = r'(Left|Right)'  # Left or Right
     match_cor = re.findall(cor_pattern, string, re.IGNORECASE)
     match_num = re.findall(num_pattern, string, re.IGNORECASE)
+    match_hand = re.findall(hand_pattern, string, re.IGNORECASE)
     # print(match_cor, match_num)
-    return (match_cor, match_num)
+    return (match_cor, match_num, match_hand)
 
+#Get the file name without percentages and model specific values
+def extract_file_name(filepath): #e.g., P01_B_pre_preprocessed
+    file_name = os.path.basename(filepath) #filename without path
+    pattern = r'(P\d+_[A-Za-z]+_pre_preprocessed)'
+    match = re.search(pattern, file_name)
+    if match:
+        return match.group(1)
 
+def matching_key_file(file1, file2): #return true if file1 and file2 are of same participant and trial
+    print(file1, file2)
+    file1_id = get_pattern_file(file1)
+    file2_id = get_pattern_file(file2)
+    if file1_id == file2_id:
+        return True
+    
+def get_pattern_file(file_name): #returns the identifying information of the file (e.g., P01_C_pre)
+    pattern = r"(P\d+_[A-Za-z]+_(pre|post))"
+    match = re.search(pattern, file_name)
+    if match:
+        print(match.group(1))
+        return match.group(1)
 # matching_key("x 21 hi", ["24 x", "21 kjh x"])
 file2 = r"C:\Users\grace\OneDrive\BCI4Kids (One Drive)\MediaPipe Done\Holistic\0.6 Detect 0.9 Track\P01\B&B_54%_P01_B_pre_preprocessed_0.6d_0.9t_0.34%.csv"
 file1 = r"C:\Users\grace\OneDrive\BCI4Kids (One Drive)\MediaPipe Done\Hand\0.1d 0.5p 1.0t\P01\B&B_20_%P01_B_pre_preprocessed_0.1d_0.5p_1t_19.0%.csv"
-combine_outputs(file1, file2)
+# combine_outputs(file1, file2)
+
+save_path = r"C:\Users\grace\OneDrive\BCI4Kids (One Drive)\MediaPipe Done\Combine Models Test Environment\Combined Outputs"
+folder1 = r"C:\Users\grace\OneDrive\BCI4Kids (One Drive)\MediaPipe Done\Combine Models Test Environment\Hand Test"
+folder2 = r"C:\Users\grace\OneDrive\BCI4Kids (One Drive)\MediaPipe Done\Combine Models Test Environment\Holistic Test"
+combine_outputs_folder(folder1, folder2, save_path)
 
         # if row.isna().all() == True: #check all cells are empty
--- Analysis/tempCodeRunnerFile.py
+++ Analysis/tempCodeRunnerFile.py
@@ -1,10 +1,1 @@
- row in file1_df.iloc[1:].iterrows(): #skip the first row because it is the header
-        #     if row.notna().any(): # check if at least one cell is filled
-        #         for header, value in zip(file1_df.columns, row):
-        #             if "Right" in header:
-        #                 if value != None: #something is detected for the right hand
-        #                     if header in all_keys: #this is something we want to transfer to the new csv
-        #                         d_frame[header] = value
-        #                     # print(header, value)
-        #     d_frame["Index"] = index
-        # print(d_frame)
+-Za-z]+_(pre|post)
